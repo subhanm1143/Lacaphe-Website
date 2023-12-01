9baf23fd421dda01dce8516ceb6747516bf49b29
RamellD <99757177+RamellD@users.noreply.github.com>
2023-12-01 12:51:45 -0800

    adding changes

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, 'STYLES')))
 app.use(express.static(path.join(__dirname, 'PHOTOS')))
 app.use(express.static(path.join(__dirname, 'ICONS')))
+app.use(express.static(path.join(__dirname, 'COMPONENTS')))
 // Set the view engine to ejs
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/PAGES'));
@@ -18,14 +19,35 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
  
   res.render('index.ejs');
+
 });app.get('/about', (req, res) => {
   
   res.render('about.ejs');
 });
+
 app.get('/login', (req, res) => {
   
   res.render('login.ejs');
 });
+app.get('/drinks', (req, res) => {
+  
+  res.render('drinks.ejs');
+});
+
+app.get('/drinks/list', (req, res) => {
+  let type = req.query.type;
+  // console.log(type);
+  db.getCon().query('SELECT * FROM testtable WHERE type = ?', [type], (err, result) => {
+    if (err) {
+      res.status(500).send('Database Error :(');
+      console.error(err);
+      return;
+    }
+    res.json(result);
+  });
+});
+
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });

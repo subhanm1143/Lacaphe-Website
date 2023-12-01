b1aa826061424cc0f80b781992bc1f711324c292
RamellD <99757177+RamellD@users.noreply.github.com>
2023-12-01 11:52:16 -0800

    testing

--- DATABASE/database.js
+++ DATABASE/database.js
@@ -1,56 +1,41 @@
 
-var mysql = require('mysql2')
-const dotenv = require('dotenv')
-dotenv.config({path: './.env'})
-// Create a MySQL connection
-const con = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-});
-// Function to establish a connection to the database
-function connectToDatabase(callback) {
-  con.connect(function(err) {
-    if (err) {
-      console.error("Error connecting to local MySQL server:", err);
-      return callback(err);
-    }
-    console.log("Connected to local MySQL server!");
-    callback(null);
+var mysql = require('mysql2');
+var con;
+function connectToDatabase() {
+  con = mysql.createConnection({
+    host: "localhost",
+    user: "liberio",
+    password: "DemBoys"
+  });
+  con.connect(err => {
+    setupDatabase(err);
   });
 }
 
-function setupDatabase(callback) {
-  con.query("CREATE DATABASE IF NOT EXISTS lacaphedb", function(err) {
-    if (err) {
-      console.error("Error creating database:", err);
-      return callback(err);
-    }
-    console.log("Database created");
+function setupDatabase(err) {
+  if (err) throw err;
+  console.log("Connected to local mysql server!");
 
-    con.query("USE lacaphedb", function(err) {
-      if (err) {
-        console.error("Error using database:", err);
-        return callback(err);
-      }
+  con.query("CREATE DATABASE IF NOT EXISTS lacaphedb")
+  if (err) throw err;
+  console.log("Database created");
+  con.query("use lacaphedb")
+  if (err) throw err;
 
-      var dropsql = "DROP TABLE IF EXISTS testtable";
-      con.query(dropsql, function(err) {
-        if (err) {
-          console.error("Error dropping table:", err);
-          return callback(err);
-        }
-        console.log("Test table dropped if it exists");
+  var dropsql = "DROP TABLE IF EXISTS testtable"
+  con.query(dropsql, function (err) {
+    if (err) throw err;
+    console.log("Test table dropped if it exits");
+  })
 
-        var tablesql = "CREATE TABLE testtable(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), image VARCHAR(255), price DECIMAL(5,2), type VARCHAR(2))";
-        con.query(tablesql, function(err) {
-          if (err) {
-            console.error("Error creating table:", err);
-            return callback(err);
-          }
-          console.log("Test table created");
+  // 's' = signature drinks, 'c' = coffee, 't' = tea, 'i' = ice blended
+  var tablesql = "CREATE TABLE testtable(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), image VARCHAR(255), price DECIMAL(5,2), type VARCHAR(2))"
+  con.query(tablesql, function (err) {
+    if (err) throw err;
+    console.log("Test table created");
+  })
 
-           var insertsql = "INSERT INTO testtable (name, image,price,type) VALUES('boba tea','https://www.unionsquareawards.org/wp-content/uploads/2019/09/images3904-5d882e0c1594c.jpg',2.50,'t'),"
+  var insertsql = "INSERT INTO testtable (name, image,price,type) VALUES('boba tea','https://www.unionsquareawards.org/wp-content/uploads/2019/09/images3904-5d882e0c1594c.jpg',2.50,'t'),"
     + "('lychee Tea','https://s3-media0.fl.yelpcdn.com/bphoto/Z0nZF9zYTaMVT5nbbGuxDA/o.jpg',3.75, 't'),"
     + "('Test Coffee','https://images.immediate.co.uk/production/volatile/sites/30/2020/08/flat-white-3402c4f.jpg?quality=90&webp=true&resize=500,454',5.00, 'c'),"
     + "('Some Signature Drink', 'http://1.bp.blogspot.com/-JFw1MGN5OoM/UD-BpCdlxzI/AAAAAAAAEMQ/brNZEjdhZWU/s1600/Lychee%2BMartini%2B3fc.jpg', 5.00, 's'),"
@@ -59,30 +44,15 @@ function setupDatabase(callback) {
     if (err) throw err;
     console.log("Test table filled");
   })
-          con.query(insertsql, function(err) {
-            if (err) {
-              console.error("Error inserting data:", err);
-              return callback(err);
-            }
-            console.log("Test table filled");
-
-            var getsql = "SELECT * FROM testtable";
-            con.query(getsql, function(err, result) {
-              if (err) {
-                console.error("Error selecting data:", err);
-                return callback(err);
-              }
-              const data = JSON.stringify(result);
-              console.log(data);
-              callback(null, data); // Successful completion
-            });
-          });
-        });
-      });
-    });
-  });
+  var getsql = "SELECT * from testtable"
+  con.query(getsql, function (err, result) {
+    if (err) console.log(err);
+    else {
+      const data = JSON.stringify(result)
+      console.log(data)
+    }
+  })
 }
-
 
 /**
  * Function that gets the connection instance

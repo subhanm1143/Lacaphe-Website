6d3cd06bb635594e1c371a0695f967aa4230cbf1
subhanm1143 <subhanm1143@gmail.com>
2024-03-07 20:00:31 -0800

    LW-120,124 Create Account Email and Password standards

--- server.js
+++ server.js
@@ -13,8 +13,6 @@ const port = 3000;
 const db = require('./DATABASE/database'); 
 const path = require('path');
 const config = require("./CONFIG/auth.config");
-var jwt = require("jsonwebtoken");
-const cookieParser = require('cookie-parser');
 const {verifyToken} = require('./MIDDLEWARE/authjwt.js');
 app.use(express.static(path.join(__dirname, 'STYLES')))
 app.use(express.static(path.join(__dirname, 'PHOTOS')))

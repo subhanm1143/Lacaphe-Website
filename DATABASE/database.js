f12bed7c97c2ac8dfea5d1a87f1cb685b194e3fd
Liberio Mukdani <liberio@mukdani.com>
2024-03-10 12:21:14 -0700

    fixing error

--- DATABASE/database.js
+++ DATABASE/database.js
@@ -1,10 +1,13 @@
 
 var mysql = require('mysql2');
 var con;
+const config = require("../CONFIG/db.config.js");
+
 function connectToDatabase() {
   con = mysql.createConnection({
+
     host: "localhost",
-    user: "liberio",
+    user: "root",
     password: "DemBoys"
   });
   con.connect(err => {
